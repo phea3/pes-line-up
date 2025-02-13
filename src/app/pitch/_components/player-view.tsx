d80f059d1d032443f96515328356eabd55231699
phea3 <sokphealoun9@gmail.com>
2025-02-13 16:37:00 +0700

    update

--- src/app/pitch/_components/player-view.tsx
+++ src/app/pitch/_components/player-view.tsx
@@ -2,86 +2,85 @@
 
 import React from "react";
 import { players } from "@/constants/players";
-import {
-  Avatar,
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Chip,
-  Divider,
-} from "@nextui-org/react";
-import { Shirt } from "lucide-react";
+import { Avatar, Badge, Card, CardBody, CardHeader } from "@nextui-org/react";
 
-type Props = {
-  order: number;
+export type PlayerType = {
+  id: number;
+  index: number;
+  name: string;
+  age: number;
+  rate: number;
+  position: "GK" | "CB" | "RB" | "LB" | "DM" | "CM" | "LW" | "RW" | "CF";
+  img: string;
+  color: "default" | "primary" | "secondary" | "success" | "warning" | "danger";
+  shirtNumber: number;
 };
 
-export default function PlayerView({ order }: Props) {
-  const [isFollowed, setIsFollowed] = React.useState(false);
+const positions: Record<string, string> = {
+  GK: "Goalkeeper",
+  CB: "Defender",
+  RB: "Defender",
+  LB: "Defender",
+  CM: "Midfielder",
+  DM: "Midfielder",
+  LW: "Forward",
+  RW: "Forward",
+  CF: "Forward",
+};
+
+export default function PlayerView() {
+  const groupedPlayers = Object.entries(
+    players.reduce<Record<string, PlayerType[]>>((acc, player) => {
+      const pos = positions[player.position];
+      acc[pos] = acc[pos] || [];
+      acc[pos].push(player);
+      return acc;
+    }, {})
+  );
 
   return (
-    <div>
-      {players.map((load) =>
-        order === load.index ? (
-          <div
-            key={load.id}
-            className="w-full flex items-center justify-center max-w-full"
-          >
-            <div className="flex flex-col gap-1 items-center ">
-              <Card className="p-1 shadow-none border">
-                <CardHeader className="justify-between">
-                  <div className="flex gap-3">
-                    <Badge
-                      size="sm"
-                      color={load.color}
-                      variant="faded"
-                      content={load.rate} // The number to display
-                      placement="top-right" // Adjust position
-                      className="!absolute" // Customize if needed
-                    >
-                      <Avatar
-                        isBordered
-                        color={load.color}
-                        radius="full"
-                        src={load.img}
-                        className="w-10 h-10"
-                      />
-                    </Badge>
-                    <Button
-                      startContent={<Shirt size={14} />}
-                      className={
-                        isFollowed
-                          ? "bg-transparent text-foreground border-default-200"
-                          : ""
-                      }
-                      color={load.color}
-                      radius="full"
-                      size="sm"
-                      variant={isFollowed ? "bordered" : "solid"}
-                      onPress={() => setIsFollowed(!isFollowed)}
-                    >
-                      {load.shirtNumber}
-                    </Button>
-                  </div>
-                </CardHeader>
-                <CardBody className="px-1 py-0 text-small">
-                  <div className="text-sm">Name: {load.name}</div>
-                  <div className="text-sm mb-4">Age: {`(${load.age}y)`}</div>
-                </CardBody>
-                <Divider />
-                <CardFooter className="py-1">
-                  <Chip color={load.color} variant="dot" size="sm">
-                    <div className="text-sm">{load.positiom}</div>
-                  </Chip>
-                </CardFooter>
-              </Card>
-            </div>
+    <div className="flex flex-col items-center">
+      {groupedPlayers.map(([position, group]) => (
+        <div key={position} className="flex gap-4 my-4">
+          {group.map((player) => (
+            <Card key={player.id}>
+              <CardHeader className="justify-center">
+                <Badge
+                  size="sm"
+                  color={player.color}
+                  variant="shadow"
+                  content={player.rate}
+                  placement="top-right"
+                  className="!absolute"
+                >
+                  <Avatar
+                    isBordered
+                    color={player.color}
+                    radius="full"
+                    src={player.img}
+                    className="w-20 h-20"
+                  />
+                </Badge>
+              </CardHeader>
+              <CardBody className="text-center">
+                <div className="font-bold">
+                  {player.name} ({player.age})
+                </div>
+                <div className="text-sm">{player.shirtNumber}</div>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+      ))}
+
+      <div className="shadow-sm absolute left-2 bg-white rounded-sm p-4 text-left">
+        <div className="text-lg font-bold">Total: {players.length} players</div>
+        {groupedPlayers.map(([position, group]) => (
+          <div key={position} className="text-sm">
+            {position}: {group.length} players
           </div>
-        ) : null
-      )}
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/pitch/page.tsx
+++ src/app/pitch/page.tsx
@@ -3,11 +3,9 @@ import PlayerView from "./_components/player-view";
 export default function PitchPage() {
   return (
     <div className="w-full h-full relative">
-      <div className="max-w-4xl mx-auto border bg-green-700">
-        <div className="grid grid-cols-5 gap-2 p-2">
-          {Array.from({ length: 50 }).map((_, index) => {
-            return <PlayerView key={index} order={index} />;
-          })}
+      <div className="max-w-full mx-auto border bg-green-700">
+        <div className="grid grid-cols-1 gap-2 p-2">
+          <PlayerView />
         </div>
       </div>
     </div>
